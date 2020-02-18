1cca07672a0791f9f0cf317e1507b8db016c1d40
guhailong18 <1097629344@qq.com>
2020-02-18 20:10:39 +0800

    Site updated: 2020-02-18 20:10:33

--- js/plugins.js
+++ js/plugins.js
@@ -1,8 +1,18 @@
+var fs = require('fs');
+var version = 'latest'
+fs.readFile('./BUILD_NUMBER.txt', function (error, data) {
+    if (error) {
+      console.log('load BUILD_NUMBER.txt fails, ' + error)
+    } else {
+        version = data.toString().trim();
+    }
+});
+
 const source = (path, cache, ext) => {
     if (cache) {
         const minFile = `${path}${ext === '.js' ? '.min' : ''}${ext}`;
         const jsdelivrCDN = hexo.config.jsdelivr;
-        return jsdelivrCDN.enable ? `//${jsdelivrCDN.baseUrl}/gh/${jsdelivrCDN.gh_user}/${jsdelivrCDN.gh_repo}@latest/${minFile}` : `${minFile}?v=${version}`
+        return jsdelivrCDN.enable ? `//${jsdelivrCDN.baseUrl}/gh/${jsdelivrCDN.gh_user}/${jsdelivrCDN.gh_repo}@${version}/${minFile}` : `${minFile}?v=${version}`
     } else {
         return path + ext
     }
